4b1b64b41ad35e2e221850f04c387b2f7d45b1f8
Arnar F. Ástvaldsson <arnarfreyr29@gmail.com>
2020-09-29 09:04:28 +0200

    Yep

---
+++ Assets/Scripts/OrderLayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrderLayer : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        sr.sortingOrder = 6;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        sr.sortingOrder = 4;
+    }
+}
