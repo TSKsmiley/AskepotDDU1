b8e833b4a244f4f760d5e64598d31054868a18e6
Arnar F. Ástvaldsson <arnarfreyr29@gmail.com>
2020-09-29 14:56:15 +0200

    lesGoo

---
+++ Assets/Scripts/MissionController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionController : MonoBehaviour
+{
+    public NPCconversation[] Missions;
+    public int CurrentMission;
+
+    public void Next()
+    {
+        CurrentMission++;
+        if (Missions.Length > CurrentMission)
+        {
+            Missions[CurrentMission].enabled = true;
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentMission = 0;
+        Missions[CurrentMission].enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
