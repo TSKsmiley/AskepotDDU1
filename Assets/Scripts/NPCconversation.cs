1217020925b1ef65897b8f382c120efcf2a16420
Arnar F. Ástvaldsson <arnarfreyr29@gmail.com>
2020-09-29 09:04:14 +0200

    Fixed Ordering Layer

--- Assets/Scripts/NPCconversation.cs
+++ Assets/Scripts/NPCconversation.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +8,12 @@ public class NPCconversation : MonoBehaviour
 
     public BoxCollider2D npcTrigger;
     public string[] Dialouge;
-    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
