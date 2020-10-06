c7933eb3bfd8084b2631092faf2a54dd19c5e39e
BenjaminHvidberg <benjaminboegelhvidberg@gmail.com>
2020-10-06 15:57:54 +0200

    Walk Sound

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
+using UnityEngine.Audio;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
 
     private Vector2 _movement;
+
+	public AudioSource audio;
 
     public Animator animator;
     private static readonly int Horizontal = Animator.StringToHash("Horizontal");
@@ -27,10 +30,19 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat(Horizontal, _movement.x);
         animator.SetFloat(Vertical, _movement.y);
         animator.SetFloat(Speed, _movement.sqrMagnitude );
-    }
+		
+		if ((Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical")) && !audio.isPlaying)
+			audio.Play();
+		else if (!Input.GetButton("Horizontal") && !Input.GetButton("Vertical") && audio.isPlaying)
+			audio.Stop(); // or Pause()
+
+
+	}
 
     void FixedUpdate() // movement 
     {
         rb.MovePosition(rb.position + _movement * (moveSpeed * Time.fixedDeltaTime));
     }
+
+
 }
