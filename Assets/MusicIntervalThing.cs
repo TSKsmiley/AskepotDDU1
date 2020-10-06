357343160e97c12bbb81d786ae26058b3dbf6a60
BenjaminHvidberg <benjaminboegelhvidberg@gmail.com>
2020-10-06 14:37:03 +0200

    dadadadadada

---
+++ Assets/MusicIntervalThing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicIntervalThing : MonoBehaviour
+{
+
+	[SerializeField] private AudioClip[] clips;
+	private int clipIndex;
+	public AudioSource audio;
+
+	void Start()
+	{
+		StartCoroutine(IPlaySound());
+	}
+
+	IEnumerator IPlaySound()
+	{
+		yield return new WaitForSeconds(Random.Range(60f, 90f));
+
+		clipIndex = Random.Range(0, clips.Length);
+		audio.PlayOneShot(clips[clipIndex], 1f);
+
+		yield return new WaitForSeconds(clips[clipIndex].length);
+		StartCoroutine(IPlaySound());
+	}
+}
