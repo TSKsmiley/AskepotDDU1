edea40b1cff6ddb4c4d3166dbfb785443a9ff005
BenjaminHvidberg <benjaminboegelhvidberg@gmail.com>
2020-10-06 13:53:06 +0200

    adadada

---
+++ Assets/VideoEndScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoEndScript : MonoBehaviour
+{
+	public VideoPlayer video;
+
+	void Start()
+	{
+		video.loopPointReached += CheckOver;
+	}
+
+	void CheckOver(VideoPlayer vp)
+	{
+		SceneManager.LoadScene(2);
+	}
+}
