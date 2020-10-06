b741f23657f6a8b2e470057ef0d93fdd2d16de21
Arnar F. Ástvaldsson <arnarfreyr29@gmail.com>
2020-10-06 10:24:40 +0200

    AHHHHHHHHHHHHHHH

---
+++ Assets/MiniGame.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MiniGame : MonoBehaviour
+{
+    public bool isActive;
+    public int missionClicks;
+    public String text;
+    public TextMeshProUGUI statusText;
+    
+    private int _clicks;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!enabled) return;
+        isActive = true;
+        statusText.text = text;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!enabled) return;
+        isActive = false;
+        statusText.text = null;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && isActive)
+        {
+            _clicks++;
+            if (_clicks >= missionClicks)
+            {
+                statusText.text = null;
+                enabled = false;
+            }
+        }
+    }
+}
